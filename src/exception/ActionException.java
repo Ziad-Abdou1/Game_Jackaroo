ae7c48b9bdd37b7cdca69beb960a6489e2891423
Morkos Shenouda Mehareb <markosshenouda@gmail.com>
2025-04-01 21:05:14 +0200

    test adding comment
    added comment

--- src/exception/ActionException.java
+++ src/exception/ActionException.java
@@ -5,7 +5,7 @@ public abstract class ActionException extends GameException {
 	public ActionException() {
         super();
     }
-	
+	//test
     public ActionException(String message) {
         super(message);
     }
